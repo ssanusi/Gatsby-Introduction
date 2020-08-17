693cbe238d5f963e1f817d37c91b9041cc7ffb5d
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-08-17 11:51:52 +0100

    added background image to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,23 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Sulaiman!</p>
-      <Link to="/about/">Learn more about me &rarr;</Link>
-      <h2>Read My Blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h1>Home</h1>
+        <p>Hello Sulaiman!</p>
+        <Link to="/about/">Learn more about me &rarr;</Link>
+        <h2>Read My Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
